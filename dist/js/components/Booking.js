3e3f8b2aeb15a137a67cd7db62c3a97d87064aac
Piotr Węgłowski <weglowskipiotr@gmail.com>
2023-09-13 15:50:16 +0200

    Add date and hours widget

--- dist/js/components/Booking.js
+++ dist/js/components/Booking.js
@@ -1,5 +1,7 @@
 import { templates, select } from '../settings.js';
 import AmountWidget from './AmountWidget.js';
+import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 
 class Booking {
   constructor(element) {
@@ -19,11 +21,19 @@ class Booking {
     thisBooking.dom.hoursAmount = element.querySelector(
       select.booking.hoursAmount
     );
+    thisBooking.dom.inputDatePicker = element.querySelector(
+      select.widgets.datePicker.wrapper
+    );
+    thisBooking.dom.inputHourPicker = element.querySelector(
+      select.widgets.hourPicker.wrapper
+    );
   }
   initWidgets() {
     const thisBooking = this;
     thisBooking.peopleWidget = new AmountWidget(thisBooking.dom.peopleAmount);
     thisBooking.hoursWidget = new AmountWidget(thisBooking.dom.hoursAmount);
+    thisBooking.datePicker = new DatePicker(thisBooking.dom.inputDatePicker);
+    thisBooking.hourPicker = new HourPicker(thisBooking.dom.inputHourPicker);
   }
 }
 export default Booking;
